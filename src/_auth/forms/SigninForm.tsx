16df8a21be4b3aab841a54c7cbfdd240ab662a7e
mithindev <mithindev1@gmail.com>
2024-11-15 15:57:23 +0530

    updated

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -53,7 +53,7 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-      <img src="/assets/images/logo-full.svg" alt="logo" width={150} height={150} />
+      {/* <img src="/assets/images/logo-full.svg" alt="logo" width={150} height={150} /> */}
 
         <h2 className="h3-bold md:h2-bold pt-3">
           Log in to your account
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -85,13 +85,13 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo-full.svg" alt="logo" width={150} height={100} />
+        {/* <img src="/assets/images/logo-full.svg" alt="logo" width={150} height={100} /> */}
 
         <h2 className="h3-bold md:h2-bold pt-3">
           Create a new account
         </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use Thottil, Please enter your details
+          Please enter your details
         </p>
 
         <form
