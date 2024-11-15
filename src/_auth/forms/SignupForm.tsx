4892fb7ee9c6f612d278c29497084d1f07ccd4f5
mithindev <mithindev1@gmail.com>
2024-11-15 15:59:41 +0530

    updated

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -85,7 +85,6 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        {/* <img src="/assets/images/logo-full.svg" alt="logo" width={150} height={100} /> */}
 
         <h2 className="h3-bold md:h2-bold pt-3">
           Create a new account
